aa858ca7e7ed9dfebbc56bd362d91748df81a54f
Michon van Dooren <me@maienm.com>
2024-04-14 10:26:44 +0200

    Setup flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    esp = {
+      url = "github:MaienM/nixpkgs-esp-dev-rust";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, esp, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      espPkgs = esp.packages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          espPkgs.esp-idf-full
+
+          cargo
+          clippy
+          espflash
+          ldproxy
+        ];
+      };
+    }
+  );
+}
